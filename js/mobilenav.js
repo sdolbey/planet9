8cb14d4e2eabe134765abcaac29d743b718655ee
sdolbey <steven.dolbey@gmail.com>
2020-04-10 19:22:50 -0400

    responsive and custom mobile navbar

---
+++ js/mobilenav.js
@@ -0,0 +1,15 @@
+function menuFunction() {
+
+    var bar = document.getElementById("topbar");
+    var icon = document.getElementById("menu-button");
+    var links = document.getElementById("mobile-nav-links");
+    if (links.style.display === "block") {
+        bar.style.height = "4em";
+        links.style.display = "none";
+        icon.setAttribute("src", "images/menu-white-18dp.svg");
+    } else {
+        bar.style.height = "60%";
+        links.style.display = "block";
+        icon.setAttribute("src", "images/close-white-18dp.svg");
+    }
+  }
